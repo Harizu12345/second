04b5edb456d062c2595f35fb773000fcfb6b22f4
msi <msi@DESKTOP-Q39FBF1>
2023-04-12 19:24:57 +0300

    Добавьте файлы проекта.

---
+++ second.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    system("color E9");
+    setlocale(0, "UKR"); // для кириллицы
+    cout << "I love c++ \n\n";
+    cout << "\tI love c++ \n\n";
+    cout << "\t\tI love c++ \n\n";
+    cout << "\t\t\tI love c++ \n\n";
+    cout << "\t\t\t\tI love c++ \n\n";
+
+}
